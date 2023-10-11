327c00f2a39c5c6e61d9450a62c053cbb703233f
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-11 12:03:39 +0200

    Set logout view

--- react_api/urls.py
+++ react_api/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 # All pages URLs
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
@@ -15,5 +17,4 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
-    path('', root_route),
 ]
