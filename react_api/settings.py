7c0acfb793a4fc8b50bd79df8b5369baa45df18a
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-16 15:51:16 +0200

    Removed Cors changes

--- react_api/settings.py
+++ react_api/settings.py
@@ -103,15 +103,15 @@ MIDDLEWARE = [
 ]
 
 
-#if 'CLIENT_ORIGIN' in os.environ:
- #   CORS_ALLOWED_ORIGINS = [
-  #      os.environ.get('CLIENT_ORIGIN')
-   # ]
-#else:
- #   CORS_ALLOWED_ORIGIN_REGEXES = [
-  #      r"^https://.*\.gitpod\.io$",
-   #     r"^http://localhost:3000$",
-    #]
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+        r"^http://localhost:3000$",
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
