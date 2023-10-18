ed8b719d55bbf62c78848de0f56962dcca7a2ccf
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-18 13:54:29 +0200

    Update get and create data in item and recipe

--- recipes/views.py
+++ recipes/views.py
@@ -13,18 +13,12 @@ class RecipeListView(generics.ListCreateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     
     def get_queryset(self):
-       user = self.request.user
-       if user.is_anonymous:
-        return Recipe.objects.none()  # Return an empty queryset for anonymous users
-       return Recipe.objects.filter(owner=user)
+        user = self.request.user
+        return Recipe.objects.filter(owner=user)
 
     def perform_create(self, serializer):
-      if self.request.user.is_anonymous:
-        # Handle the case of an anonymous user, e.g., return an error response
-        return Response({"detail": "Anonymous users cannot create items."}, status=400)
-
-    # Proceed with saving the owner as the current user
-      serializer.save(owner=self.request.user)
+        """Save the owner of the item as the current user."""
+        serializer.save(owner=self.request.user)
 
 class RecipeDetailedView(generics.RetrieveUpdateDestroyAPIView):
     """View for retrieving, updating, and deleting an recipe."""
